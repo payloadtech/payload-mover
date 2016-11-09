aee1dee13ce88aa4e9aad6c9a7aabaddb3d9cf4f
amingilani <amin@gilani.me>
2016-11-09 06:33:39 +0500

    Scripted to the point where it generates a deposit address

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ blinktrade
     .then(function() {
         return blinktrade.heartbeat();
     })
+    // log the heartbeat
     .then(function(heartbeat) {
         console.log(heartbeat.Latency);
     })
@@ -25,6 +26,24 @@ blinktrade
             "username": process.env.BLINKTRADE_API_KEY, // API key
             "password": process.env.BLINKTRADE_API_PASSWORD, // API password
         });
-    }).then(function(logged) {
+    })
+    // log the authentication
+    .then(function(logged) {
         console.log(logged);
+    })
+    // request a deposit
+    .then(function() {
+        return blinktrade.requestDeposit().on('DEPOSIT_REFRESH', function(deposit) {
+            return deposit;
+        });
+    })
+    // log the deposit and return the deposit address
+    .then(function(deposit) {
+        console.log('Logging Deposit Information')
+        console.log(deposit);
+        return deposit.Data.InputAddress;
+    })
+    .then(function(depositAddress){
+      console.log("This is the deposit address");
+      console.log(depositAddress);
     });

6a9215f4a502df5b74cf7d79a5eb6c7a71795e5e
amingilani <amin@gilani.me>
2016-11-09 13:56:48 +0500

    Send coinbase account balance

--- index.js
+++ index.js
@@ -9,41 +9,78 @@ var blinktrade = new BlinkTradeWS({
 });
 
 // Connect
-blinktrade
-    .connect()
-    // run a heartbeat
-    .then(function() {
-        return blinktrade.heartbeat();
+// blinktrade
+//     .connect()
+//     // run a heartbeat
+//     .then(function() {
+//         return blinktrade.heartbeat();
+//     })
+//     // log the heartbeat
+//     .then(function(heartbeat) {
+//         console.log(heartbeat.Latency);
+//     })
+//     // authenticate
+//     .then(function() {
+//         return blinktrade.login({
+//             "BrokerID": 8, // Urdubit
+//             "username": process.env.BLINKTRADE_API_KEY, // API key
+//             "password": process.env.BLINKTRADE_API_PASSWORD, // API password
+//         });
+//     })
+//     // log the authentication
+//     .then(function(logged) {
+//         console.log(logged);
+//     })
+//     // request a deposit
+//     .then(function() {
+//         return blinktrade.requestDeposit().on('DEPOSIT_REFRESH', function(deposit) {
+//             return deposit;
+//         });
+//     })
+//     // log the deposit and return the deposit address
+//     .then(function(deposit) {
+//         console.log('Logging Deposit Information')
+//         console.log(deposit);
+//         return deposit.Data.InputAddress;
+//     })
+//     .then(function(depositAddress){
+//       console.log("This is the deposit address");
+//       console.log(depositAddress);
+//     })    .catch(function(err) {
+    //     console.log(err);
+    // });
+
+
+// Coinbase
+var Promise = require("bluebird");
+
+var Client = require('coinbase').Client;
+var client = Promise.promisifyAll(new Client({
+    'apiKey': process.env.COINBASE_API_KEY,
+    'apiSecret': process.env.COINBASE_API_SECRET
+}));
+
+client
+    // grab the accounts
+    .getAccountsAsync({})
+    // return the first account
+    .then(function(accounts) {
+        return accounts[0];
     })
-    // log the heartbeat
-    .then(function(heartbeat) {
-        console.log(heartbeat.Latency);
+    // send the account balance to an address
+    .then(function(account) {
+      address = '13kcVthUJti7KEb4cQp1mH2X3aBu6DSTBz';
+      amount = account.balance.amount;
+      currency = account.balance.currency;
+
+      account.sendMoney({'to': address,
+                         'amount': amount,
+                         'currency': currency}, function(err, tx) {
+        console.log(tx);
+      });
+
+      console.log(account);
     })
-    // authenticate
-    .then(function() {
-        return blinktrade.login({
-            "BrokerID": 8, // Urdubit
-            "username": process.env.BLINKTRADE_API_KEY, // API key
-            "password": process.env.BLINKTRADE_API_PASSWORD, // API password
-        });
-    })
-    // log the authentication
-    .then(function(logged) {
-        console.log(logged);
-    })
-    // request a deposit
-    .then(function() {
-        return blinktrade.requestDeposit().on('DEPOSIT_REFRESH', function(deposit) {
-            return deposit;
-        });
-    })
-    // log the deposit and return the deposit address
-    .then(function(deposit) {
-        console.log('Logging Deposit Information')
-        console.log(deposit);
-        return deposit.Data.InputAddress;
-    })
-    .then(function(depositAddress){
-      console.log("This is the deposit address");
-      console.log(depositAddress);
+    .catch(function(err) {
+        console.log(err);
     });

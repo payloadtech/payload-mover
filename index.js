b978e23fc868e97130666feb95ab006a2bd33993
amingilani <amin@gilani.me>
2016-11-09 21:40:45 +0500

    Move into their own functions

--- index.js
+++ index.js
@@ -16,58 +16,62 @@ var coinbase = Promise.promisifyAll(new Client({
 
 // move bitcoin from Coinbase account to given address
 var transferCoinbaseBitcoinTo = function transferCoinbaseBitcoinTo(address) {
-  coinbase
-      // grab the accounts
-      .getAccountsAsync({})
-      // return the first account
-      .then(function(accounts) {
-          return accounts[0];
-      })
-      // send the account balance to the address
-      .then(function(account) {
-        amount = account.balance.amount;
-        currency = account.balance.currency;
-        account.sendMoney({'to': address,
-                           'amount': amount,
-                           'currency': currency}, function(err, tx) {
-          console.log(tx.title + " " + tx.subtitle);
+    coinbase
+    // grab the accounts
+        .getAccountsAsync({})
+        // return the first account
+        .then(function(accounts) {
+            return accounts[0];
+        })
+        // send the account balance to the address
+        .then(function(account) {
+            amount = account.balance.amount;
+            currency = account.balance.currency;
+            account.sendMoney({
+                'to': address,
+                'amount': amount,
+                'currency': currency
+            }, function(err, tx) {
+                console.log(tx.title + " " + tx.subtitle);
+            });
+        })
+        .catch(function(err) {
+            console.log(err);
         });
-      })
-      .catch(function(err) {
-          console.log(err);
-      });
 
 };
 
-// Connect
-blinktrade
-    .connect()
-    // run a heartbeat
-    .then(function() {
-        return blinktrade.heartbeat();
-    })
-    // log the heartbeat
-    .then(function(heartbeat) {
-        console.log(heartbeat.Latency);
-    })
-    // authenticate
-    .then(function() {
-        return blinktrade.login({
-            "BrokerID": 8, // Urdubit
-            "username": process.env.BLINKTRADE_API_KEY, // API key
-            "password": process.env.BLINKTRADE_API_PASSWORD, // API password
+// Fetch a deposit address for Urdubit and transfer bitcoin to it
+var depositCoinbasetoUrdubit = function depositCoinbasetoUrdubit() {
+    blinktrade
+        .connect()
+        // run a heartbeat
+        .then(function() {
+            return blinktrade.heartbeat();
+        })
+        // log the heartbeat
+        .then(function(heartbeat) {
+            console.log(heartbeat.Latency);
+        })
+        // authenticate
+        .then(function() {
+            return blinktrade.login({
+                "BrokerID": 8, // Urdubit
+                "username": process.env.BLINKTRADE_API_KEY, // API key
+                "password": process.env.BLINKTRADE_API_PASSWORD, // API password
+            });
+        })
+        // log the authentication
+        .then(function(logged) {
+            console.log(logged);
+        })
+        // request a deposit, and transfer bitcoin to it
+        .then(function() {
+            blinktrade.requestDeposit().on('DEPOSIT_REFRESH', function(deposit) {
+                transferCoinbaseBitcoinTo(deposit.Data.InputAddress);
+            });
+        })
+        .catch(function(err) {
+            console.log(err);
         });
-    })
-    // log the authentication
-    .then(function(logged) {
-        console.log(logged);
-    })
-    // request a deposit, and transfer bitcoin to it
-    .then(function() {
-        blinktrade.requestDeposit().on('DEPOSIT_REFRESH', function(deposit) {
-            transferCoinbaseBitcoinTo(deposit.Data.InputAddress);
-        });
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+};

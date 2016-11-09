15563ddc113a87e31c2640eed103b5a4e46c7443
amingilani <amin@gilani.me>
2016-11-09 06:16:55 +0500

    Login

--- index.js
+++ index.js
@@ -21,12 +21,9 @@ blinktrade
     // authenticate
     .then(function() {
         return blinktrade.login({
-            "MsgType": "BE",
-            "UserReqID": 9696784, // doesn't matter what this number is
             "BrokerID": 8, // Urdubit
-            "Username": process.env.BLINKTRADE_API_KEY, // API key
-            "Password": process.env.BLINKTRADE_API_PASSWORD, // API password
-            "UserReqTyp": "1"
+            "username": process.env.BLINKTRADE_API_KEY, // API key
+            "password": process.env.BLINKTRADE_API_PASSWORD, // API password
         });
     }).then(function(logged) {
         console.log(logged);
